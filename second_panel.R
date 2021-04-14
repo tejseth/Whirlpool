ba360db5776f15f7eeb153ffc6df1ff292ada61b
tejseth <55564043+tejseth@users.noreply.github.com>
2021-04-14 08:05:42 -0400

    Add files via upload

--- second_panel.R
+++ second_panel.R
@@ -18,13 +18,13 @@ main_power_second_panel2 <- main_power_second_panel2 %>%
 quantile(main_power_second_panel1$Value, seq(0,1, by = 0.01)) #goes from 0 to 7,264
 quantile(main_power_second_panel2$Value, seq(0,1, by = 0.01)) #goes from 0 to 3,916
 
-main_power_second_stats1 <- main_power_main_panel1 %>%
+main_power_second_stats1 <- main_power_second_panel1 %>%
   group_by(date) %>%
   summarize(count = n(),
             min_watts = min(Value),
             mean_watts = mean(Value),
             max_watts = max(Value))
-main_power_second_stats2 <- main_power_main_panel2 %>%
+main_power_second_stats2 <- main_power_second_panel2 %>%
   group_by(date) %>%
   summarize(count = n(),
             min_watts = min(Value),
@@ -36,7 +36,7 @@ main_power_second_stats <- rbind(main_power_second_stats1, main_power_second_sta
 main_power_second_stats <- main_power_second_stats %>%
   inner_join(outdoor_temp_all)
 
-cor(main_power_second_stats$mean_watts, main_power_second_stats$mean_temp) #-0.71
+cor(main_power_second_stats$mean_watts, main_power_second_stats$mean_temp) #-0.19
 
 main_power_second_stats %>%
   ggplot( aes(x=mean_temp, y=mean_watts, color="blue")) +
@@ -45,10 +45,9 @@ main_power_second_stats %>%
   scale_color_viridis(discrete = TRUE) +
   labs(x = "Average Temperature",
        y = "Main Second Panel Average Watts",
-       title = "Second Panel Watts and Temp.",
+       title = "Second Panel Watts and Temperature",
        subtitle = "Correlation coefficient of -0.71") +
   theme_minimal() +
-  theme_tej() +
-  transition_reveal(mean_temp)
+  theme_tej() 
 
 anim_save("main_power_second.gif")
